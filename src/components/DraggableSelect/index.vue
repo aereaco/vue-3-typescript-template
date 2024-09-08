1624089010af138b2a0ecc3b81a94e0a6bb1f7b4
Aerea Co <admin@aerea.co>
2024-09-07 23:25:38 -0500

    Updated draggable select to version 1.15.3 and fixed Draggable Select component to now use Element-Plus new structure

--- src/components/DraggableSelect/index.vue
+++ src/components/DraggableSelect/index.vue
@@ -1,7 +1,7 @@
 <template>
   <el-select
     ref="draggableSelect"
-    v-model="mutableValue"
+    v-model="selectVal"
     v-bind="$attrs"
     class="draggable-select"
     multiple
@@ -11,44 +11,57 @@
 </template>
 
 <script lang="ts">
+import { defineComponent, ref, onMounted, nextTick } from 'vue'
 import Sortable from 'sortablejs'
-import { defineComponent, ref, reactive } from 'vue'
 import { ElSelect } from 'element-plus'
 
 export default defineComponent({
   name: 'DraggableSelect',
   props: {
     modelValue: {
-      type: Array<string>,
+      type: Array as () => string[],
       required: true
     }
   },
-  setup(props) {
+  setup(props, { emit }) {
+    const draggableSelect = ref<typeof ElSelect | null>(null)
     const sortable = ref<Sortable | null>(null)
 
-    const mutableValue = reactive(props.modelValue)
-    return {
-      sortable,
-      mutableValue
+    const selectVal = ref([...props.modelValue])
+
+    const handleSelectChange = (value: string[]) => {
+      emit('update:modelValue', [...value])
     }
-  },
-  mounted() {
-    // watch(ref(this.mutableValue), (newValue) => this.$emit('input', [...(newValue as any[])]))
-    this.setSort()
-  },
-  methods: {
-    setSort() {
-      const draggableSelect = this.$refs.draggableSelect as typeof ElSelect
-      const el = draggableSelect.$el.querySelectorAll('.el-select__tags > span')[0] as HTMLElement
-      this.sortable = Sortable.create(el, {
-        ghostClass: 'sortable-ghost', // Class name for the drop placeholder
-        onEnd: (evt) => {
-          if (typeof evt.oldIndex !== 'undefined' && typeof evt.newIndex !== 'undefined') {
-            const targetRow = this.mutableValue.splice(evt.oldIndex, 1)[0]
-            this.mutableValue.splice(evt.newIndex, 0, targetRow)
+
+    const setSort = () => {
+      nextTick(() => {
+        if (draggableSelect.value) {
+          const el = draggableSelect.value.$el.querySelector('.el-select__selection') as HTMLElement
+
+          if (el) {
+            sortable.value = Sortable.create(el, {
+              ghostClass: 'sortable-ghost',
+              animation: 150,
+              onEnd: (evt) => {
+                if (evt.oldIndex !== undefined && evt.newIndex !== undefined) {
+                  const movedItem = selectVal.value.splice(evt.oldIndex, 1)[0]
+                  selectVal.value.splice(evt.newIndex, 0, movedItem)
+                  handleSelectChange(selectVal.value)
+                }
+              }
+            })
           }
         }
       })
+    }
+
+    onMounted(() => {
+      setSort()
+    })
+
+    return {
+      draggableSelect,
+      selectVal
     }
   }
 })
